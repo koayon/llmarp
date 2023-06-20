bfc75ff7f79f6082e7827ec1f7768f1ab16bccbe
koayon <58336584+koayon@users.noreply.github.com>
2023-06-20 18:18:38 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sentence = "React is a popular library for building user interfaces";
+    this.words = this.sentence.split(" ");
+
+    this.state = {
+      currentIndex: 0,
+      score: 0,
+      currentGuess: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      currentGuess: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.state.currentGuess === this.words[this.state.currentIndex]) {
+      this.setState((state) => ({
+        score: state.score + 1,
+        currentIndex: state.currentIndex + 1,
+        currentGuess: "",
+      }));
+    } else {
+      this.setState((state) => ({
+        currentIndex: state.currentIndex + 1,
+        currentGuess: "",
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Guess the Next Word</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your Score: {this.state.score} / {this.state.currentIndex}
+          <br></br>
+          Transformer Score: {this.state.currentIndex} /{" "}
+          {this.state.currentIndex}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {
+          <p>
+            Sentence: {this.words.slice(0, this.state.currentIndex).join(" ")}
+          </p>
+        }
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.state.currentGuess}
+          />
+          <input type="submit" value="Guess" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;

dcf6ab38155438690f62f5f240bead05cc172210
koayon <58336584+koayon@users.noreply.github.com>
2023-07-09 16:03:28 +0100

    Add wiki

--- wiki.py
+++ wiki.py
@@ -1,17 +1,38 @@
+import random
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
-random_url = requests.get("https://en.wikipedia.org/wiki/Special:Random", timeout=5)
-soup = BeautifulSoup(random_url.content, "html.parser")
-soup_return = soup.find(class_="firstHeading")
-if soup_return:
-    title = soup_return.title
-else:
-    raise ValueError("No title found")
 
-page_url = "https://en.wikipedia.org/wiki/%s" % title
-print(page_url)
+def try_wiki_article():
+    random_url = requests.get("https://en.wikipedia.org/wiki/Special:Random", timeout=5)
+    soup = BeautifulSoup(random_url.content, "html.parser")
+    soup_return = soup.find_all("p")
+    num_paras = len(soup_return)
+    random_para = soup_return[random.randint(0, num_paras - 1)]
+    if soup_return:
+        content = random_para.text
+    else:
+        raise ValueError("No content found")
 
-soup = BeautifulSoup(random_url.content, "html.parser")
-soup_return = soup.find("p")
-print(soup_return)
+    # print(len(content))
+    if len(content) < 300:
+        raise ValueError("Content too short")
+
+    return content
+
+
+def get_random_wiki_text():
+    for _ in range(25):
+        try:
+            wiki_text = try_wiki_article()
+            # Remove [number] citations with regex
+            wiki_text = re.sub(r"\[\d+\]", "", wiki_text)
+            return wiki_text
+        except ValueError:
+            continue
+
+
+if __name__ == "__main__":
+    print(get_random_wiki_text())

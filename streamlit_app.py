97df7c4ed89c56dcf7f59ae55bdd560c394e160b
koayon <58336584+koayon@users.noreply.github.com>
2023-07-12 20:36:17 +0100

    Add attention patterns to front-end

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from circuitsvis.attention import attention_patterns as cv_show_attention
 
 from funcs import (
     check_token,
@@ -6,6 +7,8 @@ from funcs import (
     get_new_sentence_tokens,
     update_textbox,
 )
+from gpt2_attention import predict_with_attention
+from text_copy import ATTENTION_CLICK, ATTENTION_EXPLAINER, INSTRUCTION_COPY
 
 st.set_page_config(
     layout="wide",
@@ -22,9 +25,10 @@ def set_game():
     st.session_state["user_score"] = 0
     st.session_state["ai_score"] = 0
     st.session_state["user_word"] = ""
-    sentence, sentence_tokens = get_new_sentence_tokens()
+    sentence, sentence_tokens = get_new_sentence_tokens(model_name="text-davinci-003")
     st.session_state["sentence"] = sentence
     st.session_state["sentence_tokens"] = sentence_tokens
+    st.session_state["attention_mode"] = False
 
 
 if "word_index" not in st.session_state:
@@ -35,14 +39,12 @@ words = st.session_state["sentence_tokens"]
 st.title("🤖 LLM-ARP")
 st.text("Playing as a Language Model")
 with st.sidebar:
-    st.write("Powered By OpenAI")
+    st.write("🧑 vs 🤖")
+    # st.write("Powered By OpenAI")
     reset_button, reveal_button = [st.button("Reset game"), st.button("Reveal text")]
     model_name = st.selectbox(
         label="Model",
-        options=[
-            "text-davinci-003",
-            "ada",
-        ],
+        options=["text-davinci-003", "ada", "gpt2 with attention"],
     )
     st.image("assets/mechanical_hands.png")
 
@@ -52,21 +54,31 @@ if reset_button:
 if reveal_button:
     st.session_state.word_index = len(words)
 
+if st.session_state.attention_mode:
+    st.session_state["gpt2_prediction_list"] = predict_with_attention(
+        st.session_state.sentence
+    )
+
 if st.session_state.word_index < len(words):
     with st.expander("Instructions"):
-        st.write(
-            "Pretend to be a Language Model. Please guess the next token in the sequence of random text from the web or Wikipedia. The sequence will start empty (guess the first word) and then you will be building up the sequence one word at a time.",
-            "Correct words will be shown with green underline. Incorrect words will be shown with red strikethrough along with correct word.",
-            "Ordinarily tokens can include spaces at the start but we'll allow tokens to be entered without spaces for this game.",
-        )
+        st.write(INSTRUCTION_COPY)
 
     word_index = st.session_state["word_index"]
     top_container = st.container()
 
     input_word = st.text_input(value="", label="Enter a word")
+
+    user_col, ai_col = st.columns(2)
+
+    if model_name == "gpt2 with attention":
+        st.session_state.attention_mode = st.checkbox("Show attention patterns 🧠")
+
+        attention_container = st.container()
+
     st.session_state.user_word = input_word.strip()
 
     user_word = st.session_state.user_word
+
     if user_word:
         top_container.subheader("Text so far:")
         top_container.progress(word_index / (len(words) - 1))
@@ -75,8 +87,12 @@ if st.session_state.word_index < len(words):
         is_word_correct = check_token(user_word, words[word_index])
 
         model_name = model_name or "ada"
-        ai_word = get_ai_next_token(words[:word_index], model_name=model_name)
+        if model_name == "gpt2 with attention":
+            ai_word = st.session_state.gpt2_prediction_list[word_index].best_guess
+        else:
+            ai_word = get_ai_next_token(words[:word_index], model_name=model_name)
         is_ai_correct = check_token(ai_word, words[word_index])
+        # TODO: Check AI score for GPT-2 case
 
         st.session_state.all_words.append(
             update_textbox(user_word, words[word_index], is_word_correct)
@@ -88,26 +104,49 @@ if st.session_state.word_index < len(words):
         st.session_state["user_score"] += 1 if is_word_correct else 0
         st.session_state["ai_score"] += 1 if is_ai_correct else 0
 
+        with attention_container:
+            if st.session_state.attention_mode:
+                # st.write(
+                #     st.session_state.gpt2_prediction_list[50].str_tokens,
+                # )
+                # st.write(
+                #     st.session_state.sentence_tokens,
+                # )
+
+                st.subheader("Attention patterns")
+                with st.expander("What are attention patterns and how to I read them?"):
+                    st.write(ATTENTION_EXPLAINER)
+
+                prediction = st.session_state.gpt2_prediction_list[word_index]
+
+                layer = st.slider("Layer", 0, 11, 0)
+                st.write("Attention pattern for layer", layer)
+                attention_html = cv_show_attention(
+                    tokens=prediction.str_tokens,
+                    attention=prediction.attention_pattern[layer],
+                ).show_code()
+                st.components.v1.html(attention_html, height=400)  # type: ignore
+
+                st.subheader(ATTENTION_CLICK)
+
+    with user_col:
+        user_component = st.chat_message("user")
+        user_score_message = f"User score {st.session_state.user_score} out of {st.session_state.word_index}"
+        user_component.write(user_score_message)
+        user_component.markdown(
+            " ".join(st.session_state.all_words), unsafe_allow_html=True
+        )
+
+    with ai_col:
+        ai_component = st.chat_message("assistant")
+        ai_score_message = (
+            f"AI score {st.session_state.ai_score} out of {st.session_state.word_index}"
+        )
+        ai_component.write(ai_score_message)
+        ai_component.markdown(
+            " ".join(st.session_state.ai_words), unsafe_allow_html=True
+        )
+
 else:
     st.write("Game over!")
     st.write(st.session_state.sentence)
-
-user_col, ai_col = st.columns(2)
-
-with user_col:
-    user_component = st.chat_message("user")
-    user_score_message = (
-        f"User score {st.session_state.user_score} out of {st.session_state.word_index}"
-    )
-    user_component.write(user_score_message)
-    user_component.markdown(
-        " ".join(st.session_state.all_words), unsafe_allow_html=True
-    )
-
-with ai_col:
-    ai_component = st.chat_message("assistant")
-    ai_score_message = (
-        f"AI score {st.session_state.ai_score} out of {st.session_state.word_index}"
-    )
-    ai_component.write(ai_score_message)
-    ai_component.markdown(" ".join(st.session_state.ai_words), unsafe_allow_html=True)

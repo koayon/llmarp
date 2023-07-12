11f5085501d8a86e26d7d93b44845584b021c81f
koayon <58336584+koayon@users.noreply.github.com>
2023-07-12 19:38:17 +0100

    small refactor

---
+++ text_copy.py
@@ -0,0 +1,5 @@
+INSTRUCTION_COPY = (
+    "Pretend to be a Language Model. Please guess the next token in the sequence of random text from the web or Wikipedia. The sequence will start empty (guess the first word) and then you will be building up the sequence one word at a time.",
+    "Correct words will be shown with green underline. Incorrect words will be shown with red strikethrough along with correct word.",
+    "Ordinarily tokens can include spaces at the start but we'll allow tokens to be entered without spaces for this game.",
+)
